f269cd623c86a2abc2985cc011d235b615d4634f
SofiaValan <sofia.valcarcel@hotmail.com>
2025-10-16 08:36:59 +0200

    Add data extraction functionality and weather data files

--- src/data-extraction.py
+++ src/data-extraction.py
@@ -2,17 +2,15 @@ import requests
 import pandas as pd
 import os
 from datetime import datetime
+from calendar import monthrange
 import json
 
 key = "X4lrW9Ng981W9NslRxwP4NuJa4mIDVu6ydb7jv69"
 
 url_stations = 'https://api.meteo.cat/xema/v1/estacions/metadades'
 
-# Extraction cities 
-# url_cities = 'https://api.meteo.cat/referencia/v1/municipis'
-# response = requests.get(url_cities, headers={"Content-Type": "application/json", "X-Api-Key": key})
+# Function 1: get_stations_metadata
  
-# Extraction stations
 response = requests.get(url_stations, headers={"Content-Type": "application/json", "X-Api-Key": key})
 
 print(f'The response code is: {response.status_code}')
@@ -20,7 +18,6 @@ print(f'Content of the response: {response.text}')
 
 if response.status_code == 200:
 
-    # Convert response to JSON
     stations = response.json()
 
     print(f"Number of stations: {len(stations)}")
@@ -33,14 +30,84 @@ if response.status_code == 200:
             all_dates.append({
                 'station_code': info_station['codi'],
                 'station_name': info_station['nom'],
-                'dataInici': info_estat['dataInici'],
-                'dataFi': info_estat['dataFi']
+                'start_date': info_estat['dataInici'],
+                'end_date': info_estat['dataFi']
             })
 
     print(f'Status: {all_dates}')
 
 else: 
-    print(f"❌ Error: {response.status_code}")
+    print(f"Error: {response.status_code}")
 
 df = pd.DataFrame(all_dates)
 df.head()
+df.info()
+
+# UTC
+df['start_date'] = pd.to_datetime(df['start_date'])
+df['end_date'] = pd.to_datetime(df['end_date'])
+
+df.info()
+
+df.loc[(df['start_date'].dt.year == 1999) & (df['end_date'].isna())]
+
+# Function 2: get_weather_metadata
+url_variables = 'https://api.meteo.cat/xema/v1/variables/mesurades/metadades'
+
+response = requests.get(url_variables, headers={"Content-Type": "application/json", "X-Api-Key": key})
+
+print(f'The response code is: {response.status_code}')
+print(f'Content of the response: {response.text}')
+
+if response.status_code == 200:
+
+    variables = response.json()
+
+    print(f"Number of stations: {len(variables)}")
+    print(f"First station: {variables[0]}")
+
+    all_variables = []
+
+    for info_variables in variables:
+        all_variables.append({"code":info_variables['codi'], 
+                             "name":info_variables['nom'],
+                             "unit":info_variables['unitat']})
+
+else: 
+    print(f"Error: {response.status_code}")
+
+print(all_variables)
+
+# Function 3: get_weather_info
+
+first_station = df['station_code'].iloc[0]
+year = 2000
+
+all_weather_data = []
+
+month = 1  # Enero
+days_in_month = monthrange(year, month)[1]
+
+# Iterar por cada día de enero
+for day in range(1, days_in_month + 1):
+    
+    # Construir URL usando el endpoint que quieres
+    url = f"https://api.meteo.cat/xema/v1/estacions/mesurades/{first_station}/{year}/{month:02d}/{day:02d}"
+    
+    print(f"📡 Día {day:02d} - URL: {url}")
+    
+    # Headers
+    headers = {
+        "Content-Type": "application/json",
+        "X-Api-Key": key
+    }
+    
+    # Hacer petición
+    response = requests.get(url, headers=headers)
+    print(f"Status: {response.status_code}")
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Ver estructura de la respuesta
+        print(f"Keys en respuesta: {list(data)}")
